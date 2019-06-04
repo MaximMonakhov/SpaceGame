e6a1c26afbe45534546f7a29ae8a1a97f91a209b
Maxim Monakhov <max.mon.vad@gmail.com>
2019-06-04 13:38:44 +0300

    Added laser weapon

---
+++ laser.js
@@ -0,0 +1,27 @@
+class Laser {
+  constructor(pos, angle){
+    this.pos = createVector(pos.x, pos.y);
+    this.velocity = p5.Vector.fromAngle(angle);
+    this.velocity.mult(10);
+  }
+
+  show(){
+    strokeWeight(2);
+    point(this.pos.x, this.pos.y);
+  }
+
+  move(){
+    this.pos.add(this.velocity);
+  }
+
+  hits(rock){
+    var d = dist(this.pos.x, this.pos.y, rock.pos.x, rock.pos.y);
+    if (d < rock.radius)
+      return true;
+    else return false;
+  }
+
+  outOfScreen(){
+    return (this.pos.x < -100 || this.pos.x > width + 100 || this.pos.y < -100 || this.pos.y > height + 100);
+  }
+}

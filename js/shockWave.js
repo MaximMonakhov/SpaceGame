cf075c6a0f8ee65a18f3b80708f029888ff8858d
Maxim Monakhov <max.mon.vad@gmail.com>
2019-06-05 13:48:53 +0300

    Added new weapon (rockets and shock wave)

---
+++ js/shockWave.js
@@ -0,0 +1,25 @@
+class ShockWave {
+  constructor(pos){
+    this.r = 0;
+    this.maxR = 100;
+  }
+
+  show(pos){
+    strokeWeight(2);
+    circle(pos.x, pos.y, this.r);
+  }
+
+  update(){
+    if (this.r < this.maxR)
+      this.r += 1;
+    else
+      shockWave = null;
+  }
+
+  hits(rock, pos){
+    var d = dist(pos.x, pos.y, rock.pos.x, rock.pos.y);
+    if (d < rock.radius + this.r/2)
+      return true;
+    else return false;
+  }
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,18 @@
-//Жизни, другое оружие, появление астеройдов, счет
+//Жизни, появление астеройдов, счет
 let rocks = [];
 let lasers = [];
+let rockets = [];
+let shockWave;
 let ship;
 let score = 0;
 let maxScore = 0;
 let rocksMaxCount = 30;
 let lifeCount = 0;
 let game = false;
+let shockReloadTime = 10000;
+let rocketReloadTime = 1000;
+let newRocket = throttle(function() {rockets.push(new Rocket(ship.pos, ship.angle));}, rocketReloadTime);
+let newShockWave = throttle(function() {shockWave = new ShockWave();}, shockReloadTime);
 
 function setup() {
   createCanvas(windowWidth+1, windowHeight);
@@ -61,6 +67,44 @@ function gameProcces(){
     }
   }
 
+  for (var i = rockets.length - 1; i >= 0; i--) {
+    rockets[i].show();
+    rockets[i].move();
+    if (rockets[i].outOfScreen())
+      rockets.splice(i, 1);
+    else {
+      for (var j = rocks.length - 1; j >= 0; j--) {
+        if (rockets[i].hits(rocks[j])) {
+            score++;
+            var scoreDiv = document.getElementById("scoreInt");
+            scoreDiv.innerHTML = score;
+            rocks.splice(j, 1);
+            rockets.splice(i, 1);
+            break;
+        }
+      }
+    }
+  }
+
+  if (shockWave){
+    shockWave.show(ship.pos);
+    shockWave.update();
+    for (var j = rocks.length - 1; j >= 0; j--) {
+      if (shockWave) {
+        if (shockWave.hits(rocks[j], ship.pos)) {
+            score++;
+            var scoreDiv = document.getElementById("scoreInt");
+            scoreDiv.innerHTML = score;
+            rocks.splice(j, 1);
+            break;
+        }
+      }
+      else {
+        break;
+      }
+    }
+  }
+
   ship.show();
   ship.turn();
   ship.update();
@@ -82,6 +126,10 @@ function keyPressed(){
   if (game) {
     if (keyCode == 32)
       lasers.push(new Laser(ship.pos, ship.angle));
+    if (keyCode == 17)
+      newRocket();
+    if (keyCode == 16)
+      newShockWave();
     if (keyCode == 27)
       endGame();
   }
@@ -135,4 +183,34 @@ function endGame(){
   scoreDiv.style.display = 'none';
   game = false;
   score = 0;
+}
+
+function throttle(func, ms) {
+
+  var isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function() {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
 }

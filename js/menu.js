5303fe5e4a3d6e91140ac5f71a49a42d3fad4404
Maxim Monakhov <50897624+MaximMonakhov@users.noreply.github.com>
2019-07-09 00:42:41 +0300

    var -> let fix

--- js/menu.js
+++ js/menu.js
@@ -3,14 +3,14 @@ let lifeCountButtons = document.getElementsByClassName("lifeCount");
 
 function clicked(button) {
   if (button.classList.contains("difficulty")) {
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       difficultyButtons[i].classList.remove('pressed');
     }
     button.classList.add("pressed");
   }
 
   if (button.classList.contains("lifeCount")) {
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
       lifeCountButtons[i].classList.remove('pressed');
     }
     button.classList.add("pressed");
